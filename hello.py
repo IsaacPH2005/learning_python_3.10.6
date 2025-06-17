a257fb2fde3be34a7e6a7b8dd8ad6c72d42a651f
IsaacPH2005 <parisacahuallpaisaac@gmail.com>
2025-06-16 23:23:14 -0400

    hello python

---
+++ hello.py
@@ -0,0 +1,6 @@
+import sys  # Importa el módulo sys para acceder a información del sistema
+
+#print(sys.version)  # Imprime la versión de Python que se está utilizando
+#print("Hello word")  # Imprime un mensaje en pantalla
+if 5 > 4:
+    print("5 es mayor que 4")

810f8b288a672b08e3370e47f425c76bf4f505e9
IsaacPH2005 <parisacahuallpaisaac@gmail.com>
2025-06-16 23:26:52 -0400

    variables

--- hello.py
+++ hello.py
@@ -2,5 +2,10 @@ import sys  # Importa el módulo sys para acceder a información del sistema
 
 #print(sys.version)  # Imprime la versión de Python que se está utilizando
 #print("Hello word")  # Imprime un mensaje en pantalla
-if 5 > 4:
-    print("5 es mayor que 4")
+
+
+# Variables
+x = 5
+y = "Hola mundo"
+print(x)
+print(y)
